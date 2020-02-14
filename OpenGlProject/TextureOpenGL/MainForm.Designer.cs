228a7745030918b196c784c453c2cb1484fd0bb8
Maxim Zimin <zim.ma2010@yandex.ru>
2020-02-14 10:29:42 +0300

    lab 4

---
+++ OpenGlProject/TextureOpenGL/MainForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TextureOpenGL
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openGLControl = new SharpGL.OpenGLControl();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openGLControl
+            // 
+            this.openGLControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.openGLControl.DrawFPS = false;
+            this.openGLControl.Location = new System.Drawing.Point(0, 0);
+            this.openGLControl.Name = "openGLControl";
+            this.openGLControl.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControl.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl.Size = new System.Drawing.Size(800, 450);
+            this.openGLControl.TabIndex = 0;
+            this.openGLControl.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl_OpenGLDraw);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 16;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.openGLControl);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openGLControl;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ OpenGlProject/TextureOpenGL/MainForm.cs
@@ -0,0 +1,72 @@
+﻿using SharpGL;
+using SharpGL.SceneGraph.Assets;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextureOpenGL
+{
+    public partial class MainForm : Form
+    {
+        readonly Color baseColor = Color.White;
+        readonly Texture texture = new Texture();
+        private int rotate = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.Load += MainForm_Load;
+            timer.Tick += (s, e) => rotate = (rotate + 1) % 360;
+            timer.Start();
+#if DEBUG
+            openGLControl.DrawFPS = true;
+#endif
+        }
+
+        private async void MainForm_Load(object sender, EventArgs e)
+        {
+            texture.Create(openGLControl.OpenGL, "Texture/texture.png");
+        }
+
+        private void openGLControl_OpenGLDraw(object sender, SharpGL.RenderEventArgs args)
+        {
+            OpenGL gl = this.openGLControl.OpenGL;
+
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            gl.ClearColor(baseColor.R, baseColor.G, baseColor.B, baseColor.A);
+            gl.LoadIdentity();
+            // a bit magic numbers
+            gl.Translate(0, 0, -1.3f);
+            gl.Color(1.0, 1.0, 1.0, 1.0);
+            gl.Rotate(rotate, 0, 0, 1);
+
+            gl.Enable(OpenGL.GL_TEXTURE_2D);
+            texture.Bind(gl);
+
+            gl.Begin(OpenGL.GL_QUADS);
+
+            var width = 0.3;
+            var height = 0.3;
+
+            var startX = -width / 2;
+            var startY = -width / 2;
+
+            gl.TexCoord(0, 1); gl.Vertex(new[] { startX, startY });
+            gl.TexCoord(1, 1); gl.Vertex(new[] { startX + width, startY });
+            gl.TexCoord(1, 0); gl.Vertex(new[] { startX + width, startY + height });
+            gl.TexCoord(0, 0); gl.Vertex(new[] { startX, startY + height });
+
+            gl.End();
+
+            gl.Disable(OpenGL.GL_TEXTURE_2D);
+
+            gl.Flush();
+        }
+    }
+}
